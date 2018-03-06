62583431efbee5510a609ab5200652af904e98f2
Hao Liu <hliudavis@gmail.com>
2018-03-06 22:35:27 +0800

    add git info inot status bar

--- git_status.py
+++ git_status.py
@@ -20,7 +20,7 @@ except ImportError:
 repo = GitRepo("/Users/Hao/Library/Application Support/Sublime Text 3/Packages/GitStatus")
 
 class GitStatusCommand(sublime_plugin.WindowCommand): #sublime_plugin.TextCommand):
-	def run(self): #, edit):
+	def run(self, view): #, edit):
 		#self.view.insert(edit, 0, str(repo.is_clean()))
 		print ("============ test ===============")
 		print (str(repo.is_clean()))
@@ -32,6 +32,18 @@ class GitStatusCommand(sublime_plugin.WindowCommand): #sublime_plugin.TextComman
 		print (repo.modified)
 
 
+		"""status bar info"""
+
+		# Clears the named status.
+		view.erase_status("info")
+
+		#The value will be displayed in the status bar, in a comma separated list of all status values, ordered by key.
+		if repo.is_clean():
+			view.set_status("info", "git: Clean")
+		else:
+			view.set_status("info", "git: Dirty")
+
+
 """
 These events are triggered by the buffer underlying the view,
 and thus the method is only called once,
@@ -39,17 +51,17 @@ with the first view as the parameter
 """
 class EventListener(sublime_plugin.EventListener):
 
-    def check(self):
+    def check(self, view):
         git_status = GitStatusCommand(sublime.active_window())
-        git_status.run()
+        git_status.run(view)
 
     def on_new(self, view):
     	# Called when a new buffer is created.
-    	self.check()
+    	self.check(view)
 
     def on_clone(self, view):
     	# Called when a view is cloned from an existing one.
-        self.check()
+        self.check(view)
 
     """
     def on_modified(self, view):
@@ -59,9 +71,9 @@ class EventListener(sublime_plugin.EventListener):
 
     def on_post_save(self, view):
     	# Called after a view has been saved.
-        self.check()
+        self.check(view)
+
 
     def on_activated(self, view):
     	# Called when a view gains input focus.
-        self.check()
-
+        self.check(view)
