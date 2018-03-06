afb767b1d666ea9adb170e802ecdf6e16e66319e
Hao Liu <hliudavis@gmail.com>
2018-03-06 20:50:15 +0800

    EventListener

--- git_status.py
+++ git_status.py
@@ -7,7 +7,6 @@ import sys
 # Append module directory on PYTHONPATH
 sys.path.append(os.path.join(os.path.dirname(__file__), "modules"))
 
-
 try:
 	from .modules import is_work_tree, GitRepo
 except ImportError:
@@ -20,10 +19,49 @@ except ImportError:
 #repo = GitRepo("C:/Users/Dean/AppData/Roaming/Sublime Text 3/Packages/GitAtomTheme")
 repo = GitRepo("/Users/Hao/Library/Application Support/Sublime Text 3/Packages/GitStatus")
 
-# print(repo.is_clean())
-# print(repo.add_del)
-# print(repo.modified)
+class GitStatusCommand(sublime_plugin.WindowCommand): #sublime_plugin.TextCommand):
+	def run(self): #, edit):
+		#self.view.insert(edit, 0, str(repo.is_clean()))
+		print ("============ test ===============")
+		print (str(repo.is_clean()))
+		print (repo.get_untracked_files())
+		print (repo.get_unstaged_files())
+		print (repo.get_staged_files())
+		print ("------------")
+		print (repo.add_del)
+		print (repo.modified)
 
-class ExampleCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.view.insert(edit, 0, str(repo.is_clean()))
+
+"""
+These events are triggered by the buffer underlying the view,
+and thus the method is only called once,
+with the first view as the parameter
+"""
+class EventListener(sublime_plugin.EventListener):
+
+    def check(self):
+        git_status = GitStatusCommand(sublime.active_window())
+        git_status.run()
+
+    def on_new(self, view):
+    	# Called when a new buffer is created.
+    	self.check()
+
+    def on_clone(self, view):
+    	# Called when a view is cloned from an existing one.
+        self.check()
+
+    """
+    def on_modified(self, view):
+    	# Called after changes have been made to a view.
+    	self.check()
+	"""
+
+    def on_post_save(self, view):
+    	# Called after a view has been saved.
+        self.check()
+
+    def on_activated(self, view):
+    	# Called when a view gains input focus.
+        self.check()
+
