67c2bcc70a12f8b73d1d4bcfe7d97417702d4d67
Hao Liu <hliudavis@gmail.com>
2018-03-06 23:32:19 +0800

    optimize code

--- git_status.py
+++ git_status.py
@@ -20,7 +20,7 @@ except ImportError:
 repo = GitRepo("/Users/Hao/Library/Application Support/Sublime Text 3/Packages/GitStatus")
 
 class GitStatusCommand(sublime_plugin.WindowCommand): #sublime_plugin.TextCommand):
-	def run(self, view): #, edit):
+	def run(self): #, edit):
 		#self.view.insert(edit, 0, str(repo.is_clean()))
 		print ("============ test ===============")
 		print (str(repo.is_clean()))
@@ -34,6 +34,9 @@ class GitStatusCommand(sublime_plugin.WindowCommand): #sublime_plugin.TextComman
 
 		"""status bar info"""
 
+		# Get view
+		view = sublime.active_window().active_view()
+
 		# Clears the named status.
 		view.erase_status("info")
 
@@ -43,7 +46,6 @@ class GitStatusCommand(sublime_plugin.WindowCommand): #sublime_plugin.TextComman
 		else:
 			view.set_status("info", "git: Dirty")
 
-
 """
 These events are triggered by the buffer underlying the view,
 and thus the method is only called once,
@@ -51,17 +53,17 @@ with the first view as the parameter
 """
 class EventListener(sublime_plugin.EventListener):
 
-    def check(self, view):
+    def check(self):
         git_status = GitStatusCommand(sublime.active_window())
-        git_status.run(view)
+        git_status.run()
 
     def on_new(self, view):
     	# Called when a new buffer is created.
-    	self.check(view)
+    	self.check()
 
     def on_clone(self, view):
     	# Called when a view is cloned from an existing one.
-        self.check(view)
+        self.check()
 
     """
     def on_modified(self, view):
@@ -71,9 +73,8 @@ class EventListener(sublime_plugin.EventListener):
 
     def on_post_save(self, view):
     	# Called after a view has been saved.
-        self.check(view)
-
+        self.check()
 
     def on_activated(self, view):
     	# Called when a view gains input focus.
-        self.check(view)
+        self.check()
